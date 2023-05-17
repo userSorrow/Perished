9b074244457b52904f855135129092cd62c2595c
Branden Chen <74114229+CommentXD@users.noreply.github.com>
2023-05-16 20:09:48 -0400

    made some changes to empty coordinates

--- core/map.py
+++ core/map.py
@@ -3,8 +3,10 @@ import random
 class Map:
     def __init__(self, width = 5, height = 5):
         self.grid = [[None for i in range(width)] for i in range(height)]
-        self.emptyCoordinates = []
+        self.emptyCoordinates = [(i,j) for i in range(width) for j in range(height)]
+        
 
     def autogenerate(self, numGens, numPallets):
         # No (2, 2), preexisting position
-        randPosition = (random.randint(0, width), )
+        randPosition = (random.randint(0, width))
+    
